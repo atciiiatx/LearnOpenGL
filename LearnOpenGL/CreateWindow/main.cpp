2952f926f7309ce044665b76861fedc528442da5
atciii <atcampbelliii@gmail.com>
2022-08-20 13:27:21 -0500

    Create a Window

---
+++ LearnOpenGL/CreateWindow/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+
+static const int width = 800;
+static const int height = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main(void) {
+  // Initialize the library
+  if (!glfwInit()) return -1;
+
+  // Window Hints
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Create a windowed mode window and its OpenGL context
+  GLFWwindow* window = glfwCreateWindow(width, height, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }  
+
+  // Setup viewport and resizing
+  glViewport(0, 0, width, height);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+  // Loop until the user closes the window
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    // render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    processInput(window);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
